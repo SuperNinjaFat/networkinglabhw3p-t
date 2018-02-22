ca35c1f656eee8c2d1b971206739e57df490d505
Paul Lindberg <35533648+paul-lindberg@users.noreply.github.com>
2018-02-22 14:35:56 -0500

    question 2. not sure if works
    merge this

--- upload_client.py
+++ upload_client.py
@@ -13,22 +13,33 @@ import argparse
 import socket
 import os
 
+
 class UploadError(Exception):
     """Error when uploading"""
     pass
 
-    
+
 class UploadClient:
     # TODO document this class and implement the specified functions
-    
-    pass # remove once have an actual implementation
-        
+    # create TCP socket here
 
 
-if __name__ == '__main__':    
+    def recv_all(self, sock, length):
+        data = b''
+        while len(data) < length:
+            more = sock.recv(length - len(data))
+            if not more:
+                raise EOFError('was expecting %d bytes but only received'
+                               ' %d bytes before the socket closed'
+                               % (length, len(data)))
+            data += more
+        return data
+
+
+if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='TCP File Uploader')
     parser.add_argument('host', help='interface the server listens at;'
-                        ' host the client sends to')
+                                     ' host the client sends to')
     parser.add_argument('-p', metavar='PORT', type=int, default=8900,
                         help='TCP port (default 8900)')
     args = parser.parse_args()
